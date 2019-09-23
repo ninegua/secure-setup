72d8927e0977cb38dccc090ea9118dcc49415207
Paul H. Liu <paul@thev.net>
2019-09-22 19:54:42 -0700

    Always reset pin for now

--- yubicopy.sh
+++ yubicopy.sh
@@ -6,7 +6,8 @@ test -z "$2" && echo $USAGE && exit 1
 NAME=$1
 EMAIL=$2
 FULLNAME="$NAME <$EMAIL>"
-export GNUPGHOME=$PWD/gnupg-sub
+export GNUPGSUB=$PWD/gnupg-sub
+export GNUPGHOME=$GNUPGSUB
 echo GNUPGHOME=$GNUPGHOME
 gpgconf --kill gpg-agent
 gpg --list-secret-keys --with-keygrip $FULLNAME
@@ -25,24 +26,24 @@ test12345=`echo 12345|gpg --passphrase "$PASS" $GPG_ARGS -q -r "$FULLNAME" -e|gp
 test "$test12345" != "12345" && echo "Cannot encrypt/decrypt (wrong PASS?). Abort!" && exit 1
 
 # Check if subkeys exist
-encr_subkey=`gpg --list-secret-keys --with-colons|sed -z -e 's/\nfpr/fpr/g' -e 's/\ngrp/grp/g'|grep ssb|grep -v nistp|cut -d: -f12,38|grep ^e|cut -d: -f2|tail -n1`
-auth_subkey=`gpg --list-secret-keys --with-colons|sed -z -e 's/\nfpr/fpr/g' -e 's/\ngrp/grp/g'|grep ssb|grep -v nistp|cut -d: -f12,38|grep ^a|cut -d: -f2|tail -n1`
-sign_subkey=`gpg --list-secret-keys --with-colons|sed -z -e 's/\nfpr/fpr/g' -e 's/\ngrp/grp/g'|grep ssb|grep -v nistp|cut -d: -f12,38|grep ^s|cut -d: -f2|tail -n1`
+encr_subkey=`gpg --list-secret-keys --with-colons "$FULLNAME"|sed -z -e 's/\nfpr/fpr/g' -e 's/\ngrp/grp/g'|grep ssb|grep -v nistp|cut -d: -f12,38|grep ^e|cut -d: -f2|tail -n1`
+auth_subkey=`gpg --list-secret-keys --with-colons "$FULLNAME"|sed -z -e 's/\nfpr/fpr/g' -e 's/\ngrp/grp/g'|grep ssb|grep -v nistp|cut -d: -f12,38|grep ^a|cut -d: -f2|tail -n1`
+sign_subkey=`gpg --list-secret-keys --with-colons "$FULLNAME"|sed -z -e 's/\nfpr/fpr/g' -e 's/\ngrp/grp/g'|grep ssb|grep -v nistp|cut -d: -f12,38|grep ^s|cut -d: -f2|tail -n1`
 test -z "$encr_subkey" && echo Encr Subkey not found. Abort! && exit 1
 test -z "$auth_subkey" && echo Auth Subkey not found. Abort! && exit 1
 test -z "$sign_subkey" && echo Sign Subkey not found. Abort! && exit 1
 
+while true; do
+
 echo 1. Make a copy of $GNUPGHOME
 GNUPGCOPY=$PWD/gnupg-yubi
-rsync --delete -C -a $GNUPGHOME/ $GNUPGCOPY/
+rsync --delete -C -a $GNUPGSUB/ $GNUPGCOPY/
 export GNUPGHOME=$GNUPGCOPY
 gpgconf --kill gpg-agent
 gpg --list-secret-keys
 
-while true; do
-
 if [ -z "$BATCH" ]; then
-  echo -n 2. Please insert your YubiKey, and press ENTER to continue...
+  echo -n "2. Please insert your YubiKey, and press ENTER to continue (existing OpenPGP setting will be reset):"
   read
   ykman list
 fi
@@ -52,12 +53,14 @@ test -z "$serials" && echo No YubiKey detected. Abort! && exit 1
 serial=`echo $serials|cut -d\  -f1`
 test "$serials" != "$serial" && echo More than one YubiKey detected. Abort! && exit 1
 
+RESET_PIN=y
 if [ -z "$BATCH" ]; then
-  echo -n "3. Do you want to reset YubiKey and change PIN (y/N)?"
-  read RESET_PIN
-else
-  # always reset PIN in BATCH mode
-  RESET_PIN=y
+  echo 3. Reset YubiKey OpenPGP settings, and change PIN.
+  # The following is commented out due to a problem in transfering keys.
+  # If there is already a key on a card, GPG will give extra prompt, which
+  # is not handled at the moment.
+  # echo -n "3. Do you want to reset YubiKey and change PIN (y/N)?"
+  # read RESET_PIN
 fi
 
 if [ "$RESET_PIN" = "y" ]; then
@@ -104,12 +107,6 @@ else
     echo
   fi
 fi 
-
-# set touch policy to ON
-sleep 2
-ykman -d $serial openpgp touch --admin-pin $ADMIN_PIN -f aut on
-ykman -d $serial openpgp touch --admin-pin $ADMIN_PIN -f enc on
-ykman -d $serial openpgp touch --admin-pin $ADMIN_PIN -f sig on
 
 # Transfer keys
 echo 4. Transfer subkeys to YubiKey
@@ -162,14 +159,22 @@ send -- "save\r"
 expect eof
 END
 
+# set touch policy to ON
+sleep 2
+ykman -d $serial openpgp touch --admin-pin $ADMIN_PIN -f aut on
+ykman -d $serial openpgp touch --admin-pin $ADMIN_PIN -f enc on
+ykman -d $serial openpgp touch --admin-pin $ADMIN_PIN -f sig on
+
 if [ -n "$BATCH" ]; then
   break
 else
-  echo "5. Do you want to setup another YubiKey (y/N)?"
+  echo -n "5. Do you want to setup another YubiKey (y/N)?"
   read REPEAT
   test "$REPEAT" != "y" && break
-  reset PIN
-  reset ADMIN_PIN
+
+  # Uncomment the following when we can properly handle non-reset-pin.
+  # unset PIN
+  # unset ADMIN_PIN
 fi
 
 done
